f792cae239b91a0a3e89920d3d84d78dff6035a1
pola kamal <polakamal82@gmail.com>
2025-11-12 23:59:16 +0200

    Add chats messages

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,8 @@ Rails.application.routes.draw do
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
   resources :applications, param: :token, only: [:index, :show, :create, :update] do
-    resources :chats, param: :number, only: [:index, :show, :create, :update]
+    resources :chats, param: :number, only: [:index, :show, :create, :update] do
+      resources :messages, param: :number, only: [:show, :create, :update]
+    end
   end
 end
