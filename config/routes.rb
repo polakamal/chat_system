ecb9702c80b4caec17199b083de61cfada328b19
pola kamal <polakamal82@gmail.com>
2025-11-13 15:17:28 +0200

    Add chats and messages routes

--- config/routes.rb
+++ config/routes.rb
@@ -4,9 +4,10 @@ Rails.application.routes.draw do
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
+  
   resources :applications, param: :token, only: [:index, :show, :create, :update] do
-    resources :chats, param: :number, only: [:index, :show, :create, :update] do
-      resources :messages, param: :number, only: [:show, :create, :update]
+    resources :chats, param: :number, only: [:index, :show, :create] do
+      resources :messages, param: :number, only: [:index, :show, :create]
     end
   end
 end
