ad9e6ae39feb0bf8319323014c3f39c685a29e4c
pola kamal <polakamal82@gmail.com>
2025-11-13 15:15:42 +0200

    Add application controller

--- app/controllers/applications_controller.rb
+++ app/controllers/applications_controller.rb
@@ -1,5 +1,6 @@
 class ApplicationsController < ActionController::API
-  
+  include ErrorHandler
+
   def index
     require 'services/applications/index'
     index = ::Services::Applications::Index.new(params)
@@ -7,36 +8,24 @@ class ApplicationsController < ActionController::API
   end
 
   # GET /applications/:token
-  def show
-    begin 
-      require 'services/applications/show'
-      show = ::Services::Applications::Show.new({token: params[:token]})
-      render json: show.to_hash, status: :ok
-    rescue ActiveRecord::RecordNotFound => e
-      render json: { error: e.message }, status: :not_found
-    end
+  def show 
+    require 'services/applications/show'
+    show = ::Services::Applications::Show.new({token: params[:token]})
+    render json: show.to_hash, status: :ok
   end
 
   def create
-    begin
-      require 'services/applications/create'
-      create = ::Services::Applications::Create.new({params: application_params})
-      create.process!
-      render json: { message: "Application created successfully" }, status: :created
-    rescue ActiveRecord::RecordInvalid => e
-      render json: { error: e.message }, status: :unprocessable_entity
-    end
+    require 'services/applications/create'
+    create = ::Services::Applications::Create.new({params: application_params})
+    create.process!
+    render json: { message: "Application created successfully" }, status: :created
   end
 
   def update
-    begin 
-      require 'services/applications/update'
-      update = ::Services::Applications::Update.new({token: params[:token], params: application_params })
-      update.process!
-      render json: { message: "Application updated successfully" }, status: :created
-    rescue ActiveRecord::RecordInvalid => e
-      render json: { error: e.message }, status: :unprocessable_entity
-    end
+    require 'services/applications/update'
+    update = ::Services::Applications::Update.new({token: params[:token], params: application_params })
+    update.process!
+    render json: { message: "Application updated successfully" }, status: :update
   end
 
   #------#
