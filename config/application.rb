5f07b556bd0d9953a55f3ed9aaa9b03a4877dfcd
pola kamal <polakamal82@gmail.com>
2025-11-13 15:17:03 +0200

    Add some application configs

--- config/application.rb
+++ config/application.rb
@@ -29,5 +29,15 @@ module ChatSystem
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
     config.active_job.queue_adapter = :sidekiq
+
+    Sidekiq.configure_server do |config|
+      config.redis = { url: ENV.fetch("REDIS_URL") { "redis://redis:6379/0" } }
+    end
+    
+    Sidekiq.configure_client do |config|
+      config.redis = { url: ENV.fetch("REDIS_URL") { "redis://redis:6379/0" } }
+    end
+
+    config.eager_load_paths << Rails.root.join('app/serializers')
   end
 end
