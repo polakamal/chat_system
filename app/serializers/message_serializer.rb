f8a82a753cbf171249c643be03aaabae37fb3b1d
pola kamal <polakamal82@gmail.com>
2025-11-13 17:35:41 +0200

    Fix a bug in MessageSerializer

--- app/serializers/message_serializer.rb
+++ app/serializers/message_serializer.rb
@@ -5,8 +5,7 @@ class MessageSerializer
   
   def as_json(*)
     {
-      content: @message.content,
-      user_id: @message.user_id,
+      body: @message.body,
       created_at: @message.created_at,
       updated_at: @message.updated_at,
       chat: ChatSerializer.new(@message.chat).as_json
